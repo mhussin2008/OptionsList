b9731159692a517c5ce4cbc99a3dd980c2b5bac1
mhussin2008 <65326535+mhussin2008@users.noreply.github.com>
2022-05-08 10:40:08 +0200

    new version

--- optionsList.dart
+++ optionsList.dart
@@ -2,26 +2,29 @@ import 'package:flutter/material.dart';
 import 'data.dart';
 
 class OptionsList extends StatefulWidget {
-  const OptionsList({Key? key, required this.options}) : super(key: key);
+  const OptionsList( {Key? key, required this.options, required this.page}) : super(key: key);
   final List<String> options;
+  final int page;
 
   @override
   State<OptionsList> createState() => _OptionsListState();
 }
 
 class _OptionsListState extends State<OptionsList> {
+
   @override
   Widget build(BuildContext context) {
+
     return Column(
         children: widget.options.map((e) =>
         ListTile(
             title:  Text(e),
             leading: Radio<String>(
               value: e,
-              groupValue: data.chosen,
+              groupValue:Data.chosen[widget.page],
               onChanged: (String? value) {
                 setState(()
-                { data.chosen = value??'false';
+                { Data.chosen[widget.page]= value??'false';
                 });},
             ),
             textColor: Colors.tealAccent,
